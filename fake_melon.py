9ea099bea1c702f0053f978f476d714de1617b4a
thanakritfluk <thanakrit.da@ku.th>
2019-05-27 17:31:12 +0700

    setup project name

--- fake_melon.py
+++ fake_melon.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
 from flask_pymongo import PyMongo, MongoClient
 
-app = Flask(__name__)
+fake_melon = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'MusicStores'
-app.config["MONGO_URI"] = "mongodb+srv://admin:1234561%40@cluster0-ngbcy.mongodb.net/MusicStores"
-mongo = PyMongo(app)
+fake_melon.config['MONGO_DBNAME'] = 'MusicStores'
+fake_melon.config["MONGO_URI"] = "mongodb+srv://admin:1234561%40@cluster0-ngbcy.mongodb.net/MusicStores"
+mongo = PyMongo(fake_melon)
 # All collections in database
 Track = mongo.db.Tracks
 Customers = mongo.db.Customers
@@ -13,9 +13,10 @@ Play_list = mongo.db.Play_list
 Transactions = mongo.db.Transactions
 
 
-@app.route('/')
+@fake_melon.route('/')
 def index():
     return 'OK!'
+
 
 # def add():
 #     # user = mongo.db.Tracks
@@ -26,4 +27,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    fake_melon.run()
