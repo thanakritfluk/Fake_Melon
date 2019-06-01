9f8997d247eb8d35e78ea5bb11a85664589635e8
Poonnanun <poonnanun.p@ku.th>
2019-06-01 11:34:32 +0700

    fix like fav

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import pymongo
 from app import fake_melon, lm, mongo
-from flask import request, redirect, render_template, url_for, flash
+from flask import request, redirect, render_template, url_for, flash, session
 from flask_login import login_user, logout_user, login_required
 from .forms import LoginForm
 from .user import User
@@ -72,33 +72,33 @@ def logout():
 def song_detail():
     name = request.form['name']
     rank = find_rank(str(name))
-    artist = 'not found'
-    genre = 'not found'
-    albums = 'not found'
-    like = '0'
-    img = 'static/image/notfound.jpg'
     track_detail = Track.find_one({"track_name": name})
-    for key, val in track_detail.items():
-        if 'track_name' in key:
-            name = val
-        if 'num_favourite' in key:
-            like = val
-        if 'artist_name' in key:
-            artist = val
-        if 'genre' in key:
-            genre = val
-        if 'albums_name' in key:
-            albums = val
-        if 'url_img' in key:
-            img = val
-    return render_template('songdetail.html', name=name, like=like, artist=artist, genre=genre, albums=albums, img=img,
-                           rank=rank)
+    like_user = None
+    fav_user = None
+    like_list = []
+    fav_list = []
+    if flask_login.current_user.is_authenticated():
+        name = Users.find_one({"username": flask_login.current_user.get_id()})
+        for key, val in name.items():
+            if '_id' in key:
+                c_user = Play_list.find_one({"_id": val})
+                if c_user is not None:
+                    for key, val in c_user.items():
+                        if 'playlist' in key:
+                            fav_user = val
+                        if 'likes' in key:
+                            like_user = val
+        for key, val in fav_user.items():
+            fav_list.append(val)
+        for key, val in like_user.items():
+            like_list.append(val)
+    return render_template('songdetail.html', data=track_detail, rank=rank, fav=fav_list, like=like_list)
 
 
 @fake_melon.route('/playlist')
 @login_required
 def playlist():
-    
+
     track_list = Track.find().sort([("num_favourite", pymongo.DESCENDING)])
     fav_user = None
     fav_list = []
@@ -118,13 +118,14 @@ def playlist():
             if i['track_name'] in fav_list:
                 t_list.append(i)
 
-    return render_template('playlist.html', track = t_list)
+    return render_template('playlist.html', track=t_list)
 
 
 @fake_melon.route('/fav', methods=['POST'])
 @login_required
 def fav():
     s_name = request.form['name']
+    search = request.form['search']
     name = Users.find_one({"username": flask_login.current_user.get_id()})
     id = 0
     for key, val in name.items():
@@ -156,13 +157,66 @@ def fav():
             "playlist": {"1": s_name},
             "likes": {}
         })
-    return redirect(url_for('home'))
+    if search == '.':
+        return redirect(url_for('home'))
+    elif search == ',':
+        rank = find_rank(str(s_name))
+        track_detail = Track.find_one({"track_name": s_name})
+        like_user = None
+        fav_user = None
+        like_list = []
+        fav_list = []
+        if flask_login.current_user.is_authenticated():
+            name = Users.find_one({"username": flask_login.current_user.get_id()})
+            for key, val in name.items():
+                if '_id' in key:
+                    c_user = Play_list.find_one({"_id": val})
+                    if c_user is not None:
+                        for key, val in c_user.items():
+                            if 'playlist' in key:
+                                fav_user = val
+                            if 'likes' in key:
+                                like_user = val
+            for key, val in fav_user.items():
+                fav_list.append(val)
+            for key, val in like_user.items():
+                like_list.append(val)
+        return render_template('songdetail.html', data=track_detail, rank=rank, fav=fav_list, like=like_list)
+    else:
+        if search == "":
+            return redirect(url_for('home'))
+        else:
+            like_user = None
+            fav_user = None
+            like_list = []
+            fav_list = []
+            if flask_login.current_user.is_authenticated():
+                name = Users.find_one(
+                    {"username": flask_login.current_user.get_id()})
+                for key, val in name.items():
+                    if '_id' in key:
+                        c_user = Play_list.find_one({"_id": val})
+                        if c_user is not None:
+                            for key, val in c_user.items():
+                                if 'playlist' in key:
+                                    fav_user = val
+                                if 'likes' in key:
+                                    like_user = val
+                for key, val in fav_user.items():
+                    fav_list.append(val)
+                for key, val in like_user.items():
+                    like_list.append(val)
+            data = Track.find(
+                {"track_name": {'$regex': str(search), '$options': 'i'}})
+            return render_template('display.html', searching=search, data=data, like=like_list, fav=fav_list)
+        return render_template('display.html')
 
 
 @fake_melon.route('/like', methods=['POST'])
 @login_required
 def like():
     s_name = request.form['name']
+    search = request.form['search']
     name = Users.find_one({"username": flask_login.current_user.get_id()})
     id = 0
     for key, val in name.items():
@@ -202,7 +256,59 @@ def like():
     like += 1
     Track.update_one({"track_name": s_name}, {
                      "$set": {"num_favourite": int(like)}})
-    return redirect(url_for('home'))
+    if search == '.':
+        return redirect(url_for('home'))
+    elif search == ',':
+        rank = find_rank(str(s_name))
+        track_detail = Track.find_one({"track_name": s_name})
+        like_user = None
+        fav_user = None
+        like_list = []
+        fav_list = []
+        if flask_login.current_user.is_authenticated():
+            name = Users.find_one({"username": flask_login.current_user.get_id()})
+            for key, val in name.items():
+                if '_id' in key:
+                    c_user = Play_list.find_one({"_id": val})
+                    if c_user is not None:
+                        for key, val in c_user.items():
+                            if 'playlist' in key:
+                                fav_user = val
+                            if 'likes' in key:
+                                like_user = val
+            for key, val in fav_user.items():
+                fav_list.append(val)
+            for key, val in like_user.items():
+                like_list.append(val)
+        return render_template('songdetail.html', data=track_detail, rank=rank, fav=fav_list, like=like_list)
+    else:
+        if search == "":
+            return redirect(url_for('home'))
+        else:
+            like_user = None
+            fav_user = None
+            like_list = []
+            fav_list = []
+            if flask_login.current_user.is_authenticated():
+                name = Users.find_one(
+                    {"username": flask_login.current_user.get_id()})
+                for key, val in name.items():
+                    if '_id' in key:
+                        c_user = Play_list.find_one({"_id": val})
+                        if c_user is not None:
+                            for key, val in c_user.items():
+                                if 'playlist' in key:
+                                    fav_user = val
+                                if 'likes' in key:
+                                    like_user = val
+                for key, val in fav_user.items():
+                    fav_list.append(val)
+                for key, val in like_user.items():
+                    like_list.append(val)
+            data = Track.find(
+                {"track_name": {'$regex': str(search), '$options': 'i'}})
+            return render_template('display.html', searching=search, data=data, like=like_list, fav=fav_list)
+        return render_template('display.html')
 
 
 @fake_melon.route('/registration', methods=['POST', 'GET'])
@@ -229,6 +335,7 @@ def registration():
             return redirect(url_for('login'))
     return render_template('regist.html')
 
+
 @fake_melon.route('/remove', methods=['POST'])
 @login_required
 def remove():
@@ -250,9 +357,10 @@ def remove():
             if s_name in val:
                 s = key
         st = "playlist.{size}".format(size=s)
-        
+
         Play_list.update_one({'_id': id}, {"$unset": {st: s_name}})
     return redirect(url_for('playlist'))
+
 
 @fake_melon.route('/search', methods=['POST', 'GET'])
 def search():
@@ -266,7 +374,8 @@ def search():
             like_list = []
             fav_list = []
             if flask_login.current_user.is_authenticated():
-                name = Users.find_one({"username": flask_login.current_user.get_id()})
+                name = Users.find_one(
+                    {"username": flask_login.current_user.get_id()})
                 for key, val in name.items():
                     if '_id' in key:
                         c_user = Play_list.find_one({"_id": val})
@@ -280,7 +389,8 @@ def search():
                     fav_list.append(val)
                 for key, val in like_user.items():
                     like_list.append(val)
-            data = Track.find({"track_name": {'$regex': str(searching), '$options': 'i'}})
+            data = Track.find(
+                {"track_name": {'$regex': str(searching), '$options': 'i'}})
             return render_template('display.html', searching=searching, data=data, like=like_list, fav=fav_list)
     return render_template('display.html')
 
